3c058258b395a77d34a11e2c42cd3aa8318d3333
Daniela Sayuri Yassuda <danielayassuda@gmail.com>
2017-07-17 22:40:50 -0300

    Allow CORS during development

--- server/index.js
+++ server/index.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+}
+
 app.get('/', function(req, res) {
   res.send('Ok');
 });
